ea6a17d4cc18034f4a4a6d5b8edc6ffc24e384b0
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-11 16:04:49 -0300

    Implementado valor por hora

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -21,6 +21,12 @@ public class RentalService {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
 		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
-	}
 
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+	}
 }
