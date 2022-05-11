544414530319921cabf6bf9252ffdd6265343f8d
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-11 15:52:55 -0300

    Variável para converter milissegundos em hora

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -17,9 +17,10 @@ public class RentalService {
 	}
 
 	public void processInvoice(CarRental carRental) {
-
+		// /1000 converte milissegundos para segundos / 60 p/ mínutos /60 p/ hora
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
 	}
 
 }
